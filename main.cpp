45727b6b4e34b2291381ba7076db963de8453352
Viktor <viktorklement04@gmail.com>
2026-02-23 17:39:39 +0100

    Hibak dokumentalasa kommentekkel

--- main.cpp
+++ main.cpp
@@ -4,23 +4,40 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    // HIBA: Hibás változónév, N_ELEMENTS helyett NELEMENTS van írva
+    int *b = new int[NELEMENTS]; 
+
+    // HIBA: Karakterkonstanst (' ') használsz szövegliterál (" ") helyett, és hiányzik a pontosvessző a végéről
+    std::cout << '1-100 ertekek duplazasa' 
+
+    // HIBA: Hiányzik a ciklusfeltétel és a növekmény
+    for (int i = 0;) 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+
+    // HIBA: A ciklusfeltétel hibás (i értéke 0, így a feltétel rögtön hamis), a ciklus nem fut le
+    for (int i = 0; i; i++) 
     {
-        std::cout << "Ertek:"
+        // HIBA: Hiányzik a kiírandó érték és a lezáró pontosvessző
+        std::cout << "Ertek:" 
     }    
+
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+
+    // HIBA: Az atlag változó nincs inicializálva (0-ra kellene állítani)
+    int atlag; 
+
+    // HIBA: Pontosvessző helyett vessző van a ciklusfejben
+    for (int i = 0; i < N_ELEMENTS, i++) 
     {
-        atlag += b[i]
+        // HIBA: Hiányzik a lezáró pontosvessző
+        atlag += b[i] 
     }
+
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    // HIBA: A dinamikusan foglalt memória (new int[]) nincs felszabadítva (delete[] b)
     return 0;
 }
